6ab2ca3f59731c426d3205578de1aba6494b21cb
Soumya Bhardwaj <soumyabhardwaj75@gmail.com>
2023-11-16 22:34:23 +0530

    Initial Commit

---
+++ MaxAssignment/MaxAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxAssignmentApp.swift
+//  MaxAssignment
+//
+//  Created by soumya bhardwaj on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MaxAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
